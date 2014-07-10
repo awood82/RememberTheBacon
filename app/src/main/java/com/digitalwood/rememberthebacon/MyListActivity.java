8a495838f2e39dd87318f7246de3f1a2347332f0
Andrew Wood <awood82@gmail.com>
2014-07-09 22:28:36 -0700

    Initial commit w/ sample app using Picasso in a ListView.

---
+++ app/src/main/java/com/digitalwood/rememberthebacon/MyListActivity.java
@@ -0,0 +1,39 @@
+package com.digitalwood.rememberthebacon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MyListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new MyListFragment();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/digitalwood/rememberthebacon/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.digitalwood.rememberthebacon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.widget.FrameLayout;
+
+public abstract class SingleFragmentActivity extends FragmentActivity
+{
+    protected static final String FRAGMENT_TAG = "SingleFragmentActivity.Fragment";
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        FrameLayout fl = new FrameLayout(this);
+        fl.setId(R.id.fragmentContainer);
+        setContentView(fl);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null)
+        {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
